0f30decc343a0efa73c0f4d326db8778eeeb65e7
Kimberly Lewis <kblewis.flt@gmail.com>
2015-05-02 11:24:02 -0500

    config.js setup

---
+++ js/config.js
@@ -0,0 +1,13 @@
+;(function(){
+  'use strict';
+  
+  angular.module('bloxApp')
+    .config(function($routeProvider){
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      
+    })
+  
+}());
